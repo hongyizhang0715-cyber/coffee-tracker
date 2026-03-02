5f39273c531b28cb97a39b66ea076033b09c528e
hongyizhang0715-cyber <hongyi.zhang0715@gmail.com>
2026-03-02 14:29:06 +0800

    Initial commit - Coffee Tracker PWA
    Initial commit - Coffee Tracker PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'coffee-brew-log-v1';
+const CACHE_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        const url = new URL(event.request.url);
+        if (url.origin === location.origin) {
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, response.clone());
+          });
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
